6271ba4b9677b82730cb2afb12c7ec90cf74d978
Ayush Verma <162732564+AyushCipher@users.noreply.github.com>
2026-01-20 15:34:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 // CORS config
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // frontend URLs
+    origin: ['https://onecart-e-commerce-frontend-md2n.onrender.com', 'http://localhost:5174'], // frontend URLs
     credentials: true, // allow cookies to be sent
   })
 );
